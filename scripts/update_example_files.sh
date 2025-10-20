71db117962c3b28117178a72a5ab632def9cd684
Rubel <rubel.mozumder@outlook.com>
2025-10-20 14:40:22 +0200

    Update ref files.

--- scripts/update_example_files.sh
+++ scripts/update_example_files.sh
@@ -12,7 +12,7 @@ echo " Root directory: ${scpt_dir} "
 echo " Updating XRD-918-16_10 data file... ${root_dir}"
 
 xrdml=${root_dir}/tests/data/xrdml_918-16_10
-find ${xrdml} -type f ! \( -name '*.log -o -name *.nxs' \) | xargs  dataconverter --nxdl NXxrd_pan --reader xrd --output ${xrdml}/XRD-918-16_10.nxs
-read_nexus -f ${xrdml}/XRD-918-16_10.nxs > ${xrdml}/ref_nexus.log 2>&1
+find ${xrdml} -type f ! \( -name '*.log -o -name *.nxs' \) | xargs  dataconverter --nxdl NXxrd_pan --reader xrd --output ${xrdml}/output.nxs
+read_nexus -f ${xrdml}/output.nxs > ${xrdml}/ref_nexus.log 2>&1
 rm ${xrdml}/XRD-918-16_10.nxs
 
